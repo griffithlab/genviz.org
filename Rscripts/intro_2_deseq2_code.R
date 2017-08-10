cd56de430b3a43fc1c71e9867b15c29bedd811d6
Zach Skidmore <zskidmor@genome.wustl.edu>
2017-08-10 15:25:50 -0500

    finnished DESeq2 section

--- Rscripts/intro_2_deseq2_code.R
+++ Rscripts/intro_2_deseq2_code.R
@@ -14,9 +14,9 @@ rawCounts <- read.delim("https://www.ebi.ac.uk/gxa/experiments-content/E-GEOD-50
 sampleData <- read.delim("https://www.ebi.ac.uk/gxa/experiments-content/E-GEOD-50760/resources/ExperimentDesignFile.RnaSeq/experiment-design")
 
 # # remove ambiguous data from individual 2 (remove)
-removeSamples <- sampleData[sampleData$Sample.Characteristic.individual == 2,"Run"]
-sampleData <- sampleData[!sampleData$Run %in% removeSamples,]
-rawCounts <- rawCounts[,!colnames(rawCounts) %in% removeSamples]
+# removeSamples <- sampleData[sampleData$Sample.Characteristic.individual == 2,"Run"]
+# sampleData <- sampleData[!sampleData$Run %in% removeSamples,]
+# rawCounts <- rawCounts[,!colnames(rawCounts) %in% removeSamples]
 
 # convert count data to a matrix of appropriate form that DEseq2 can read
 geneID <- rawCounts$Gene.ID
@@ -54,8 +54,11 @@ deseq2Data <- deseq2Data[rowSums(counts(deseq2Data)) > 5, ]
 # run pipeline for differential expression steps
 deseq2Data <- DESeq(deseq2Data)
 
-# # load the R environment with this object (optional)
-# load(differentialExression.RData)
+# load the R environment with this object  from the web (optional)
+load(url("http://genomedata.org/gen-viz-workshop/intro_to_deseq2/tutorial/deseq2Data_v1.RData"))
+
+# or directly after downloading the file (optional)
+load("deseq2Data_v1.RData")
 
 # Extract differential expression results
 deseq2Results <- results(deseq2Data, contrast=c("tissueType", "primary colorectal cancer", "normal-looking surrounding colonic epithelium"))
@@ -88,7 +91,7 @@ ggplot(deseq2ResDF, aes(baseMean, log2FoldChange, colour=padj)) + geom_point(siz
 otop2Counts <- plotCounts(deseq2Data, gene="ENSG00000183034", intgroup=c("tissueType", "individualID"), returnData=TRUE)
 
 # plot the data using ggplot2
-colourPallette <- c("#bbcfc4","#7145cd","#90de4a","#cd46c1","#77dd8e","#592b79","#d7c847","#6378c9","#619a3c","#d44473","#63cfb6","#dd5d36","#5db2ce","#8d3b28","#b1a4cb","#af8439","#c679c0","#4e703f","#753148","#cac88e","#352b48","#cd8d88","#463d25","#556f73")
+colourPallette <- c("#7145cd","#bbcfc4","#90de4a","#cd46c1","#77dd8e","#592b79","#d7c847","#6378c9","#619a3c","#d44473","#63cfb6","#dd5d36","#5db2ce","#8d3b28","#b1a4cb","#af8439","#c679c0","#4e703f","#753148","#cac88e","#352b48","#cd8d88","#463d25","#556f73")
 ggplot(otop2Counts, aes(x=tissueType, y=count, colour=individualID, group=individualID)) + geom_point() + geom_line() + theme_bw() + theme(axis.text.x=element_text(angle=15, hjust=1), ) + scale_colour_manual(values=colourPallette) + guides(colour=guide_legend(ncol=3)) + ggtitle("OTOP2")
 
 # transform count data using the variance stablilizing transform
@@ -124,7 +127,7 @@ clusterGene <- hclust(distanceGene)
 clusterSample <- hclust(distanceSample)
 
 # construct a dendogram for samples
-# install.packages("ggdendro")
+install.packages("ggdendro")
 library(ggdendro)
 sampleModel <- as.dendrogram(clusterSample)
 sampleDendrogramData <- segment(dendro_data(sampleModel, type = "rectangle"))
@@ -137,7 +140,7 @@ deseq2VST$variable <- factor(deseq2VST$variable, levels=clusterSample$labels[clu
 heatmap <- ggplot(deseq2VST, aes(x=variable, y=Gene, fill=value)) + geom_raster() + scale_fill_viridis(trans="sqrt") + theme(axis.text.x=element_text(angle=65, hjust=1), axis.text.y=element_blank(), axis.ticks.y=element_blank())
 
 # combine the dendrogram and the heatmap
-# install.packages("gridExtra")
+install.packages("gridExtra")
 library(gridExtra)
 grid.arrange(sampleDendrogram, heatmap, ncol=1, heights=c(1,5))
 
@@ -147,8 +150,8 @@ library(gtable)
 library(grid)
 
 # modify the ggplot objects
-sampleDendrogram_1 <- sampleDendrogram + scale_x_continuous(expand=c(.0085, .0085))
-heatmap_1 <- heatmap + scale_x_discrete(expand=c(0, 0))
+sampleDendrogram_1 <- sampleDendrogram + scale_x_continuous(expand=c(.0085, .0085)) + scale_y_continuous(expand=c(0, 0))
+heatmap_1 <- heatmap + scale_x_discrete(expand=c(0, 0)) + scale_y_discrete(expand=c(0, 0))
 
 # convert both grid based objects to grobs
 sampleDendrogramGrob <- ggplotGrob(sampleDendrogram_1)
@@ -173,6 +176,28 @@ finalGrob <- arrangeGrob(sampleDendrogramGrob, heatmapGrob, ncol=1, heights=c(2,
 # draw the plot
 grid.draw(finalGrob)
 
+# re-order the sample data to match the clustering we did
+sampleData_v2 <- sampleData
+sampleData_v2$Run <- factor(sampleData_v2$Run, levels=clusterSample$labels[clusterSample$order])
+
+# construct a plot to show the clinical data
+colours <- c("#84A5D6", "#F69275", "#B91E6F")
+sampleClinical <- ggplot(sampleData_v2, aes(x=Run, y=1, fill=Factor.Value.clinical.information.)) + geom_tile() + scale_x_discrete(expand=c(0, 0)) + scale_y_discrete(expand=c(0, 0)) + scale_fill_manual(name="Tissue", values=colours) + theme_void()
+
+# convert the clinical plot to a grob
+sampleClinicalGrob <- ggplotGrob(sampleClinical)
+
+# make sure every width between all grobs is the same
+maxWidth <- unit.pmax(sampleDendrogramGrob$widths, heatmapGrob$widths, sampleClinicalGrob$widths)
+sampleDendrogramGrob$widths <- as.list(maxWidth)
+heatmapGrob$widths <- as.list(maxWidth)
+sampleClinicalGrob$widths <- as.list(maxWidth)
+
+# arrange and output the final plot
+finalGrob <- arrangeGrob(sampleDendrogramGrob, sampleClinicalGrob, heatmapGrob, ncol=1, heights=c(2,1,5))
+grid.draw(finalGrob)
+
+
 ################################################################################
 ############################## Exercise 1 code #################################
 ################################################################################
@@ -185,7 +210,7 @@ geneModel <- as.dendrogram(clusterGene)
 geneDendrogramData <- segment(dendro_data(geneModel, type = "rectangle"))
 
 # construct the dendrogram in ggplot
-geneDendrogram <- ggplot(geneDendrogramData) + geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) + coord_flip() + scale_y_reverse(expand=c(0, 0)) + theme_dendro()
+geneDendrogram <- ggplot(geneDendrogramData) + geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) + coord_flip() + scale_y_reverse(expand=c(0, 0)) + scale_x_continuous(expand=c(0, 0)) + theme_dendro()
 
 ################################################################################
 ################# Step 2: Re-arrange the heatmap cells #########################
@@ -209,7 +234,10 @@ heatmapGrob <- ggplotGrob(heatmap + scale_x_discrete(expand=c(0, 0)) + scale_y_d
 geneDendrogramGrob <- ggplotGrob(geneDendrogram + scale_x_discrete(expand=c(0, 0)))
 
 # we already have a sample Dendrogram, but here it is again
-sampleDendrogramGrob <- ggplotGrob(sampleDendrogram + scale_x_continuous(expand=c(.0085, .0085)))
+sampleDendrogramGrob <- ggplotGrob(sampleDendrogram + scale_x_continuous(expand=c(.0085, .0085)) + scale_y_continuous(expand=c(0, 0)))
+
+# we already have our sample clinical plot but here it is again
+sampleClinicalGrob <- sampleClinicalGrob
 
 ################################################################################
 ######### Step 4: align the gene dendrograms to match the heatmap ##############
@@ -223,22 +251,24 @@ geneDendrogramGrob$heights <- as.list(maxHeight)
 heatmapGrob$heights <- as.list(maxHeight)
 
 ################################################################################
-## Step 4b: we have a new heatmap so we need to re-align the sample dendrogram #
+# Step 4b: we have a new heatmap so we need to re-align the horizontal elements #
 
 # repeat the steps in the tutorial
 
 # check the widths of each grob
 sampleDendrogramGrob$widths
 heatmapGrob$widths
+sampleClinicalGrob$widths
 
 # add in the missing columns
 sampleDendrogramGrob <- gtable_add_cols(sampleDendrogramGrob, heatmapGrob$widths[7], 6)
 sampleDendrogramGrob <- gtable_add_cols(sampleDendrogramGrob, heatmapGrob$widths[8], 7)
 
-# make sure every width between the two grobs is the same
-maxWidth <- unit.pmax(sampleDendrogramGrob$widths, heatmapGrob$widths)
+# make sure every width between all grobs is the same
+maxWidth <- unit.pmax(sampleDendrogramGrob$widths, heatmapGrob$widths, sampleClinicalGrob$widths)
 sampleDendrogramGrob$widths <- as.list(maxWidth)
 heatmapGrob$widths <- as.list(maxWidth)
+sampleClinicalGrob$widths <- as.list(maxWidth)
 
 ################################################################################
 ############### Step 5: create a blank panel ###################################
@@ -250,7 +280,7 @@ blankPanel <- grid.rect(gp=gpar(col="white"))
 ############### Step 6: Arrange the final result ###############################
 
 # arrange all the plots together
-finalGrob_v2 <- arrangeGrob(blankPanel, sampleDendrogramGrob, geneDendrogramGrob, heatmapGrob, ncol=2, nrow=2, widths=c(1,5), heights=c(2,5))
+finalGrob_v2 <- arrangeGrob(blankPanel, sampleDendrogramGrob, blankPanel, sampleClinicalGrob, geneDendrogramGrob, heatmapGrob, ncol=2, nrow=3, widths=c(1,5), heights=c(2,.8,6))
 
 # draw the final result
 grid.draw(finalGrob_v2)
